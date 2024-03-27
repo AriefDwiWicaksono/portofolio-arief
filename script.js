7f7648d01a847464b416adefb862b2d89f8c3d3d
AriefDwiWicaksono <tugasarief997@gmail.com>
2024-03-27 10:49:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function whatsApp() {
+    var nama = document.getElementById('nama').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var message = document.getElementById('message').value;
+
+    var whatsapp = "https://wa.me/6282113285557?text="
+    + "Nama: " + nama + "%0a"
+    + "Email: " + email + "%0a"
+    + "Number: " + number + "%0a"
+    + "Message: " + message;
+
+    window.open(whatsapp,"_blank").focus();
+}
